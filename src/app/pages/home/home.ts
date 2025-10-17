ca880abe362e4b914bcc7a048c51ad383641527b
samuelmota321 <samuel.mota.2010@gmail.com>
2025-10-17 13:10:23 -0300

    card component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Card } from '../../components/card/card';
+
+interface TutorialCard {
+  index: number;
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    Card
+  ],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
+  standalone: true 
 })
 export class Home {
-
+  tutorialCards: TutorialCard[] = [
+    { index: 1, title: "Slides Atrativos", description: "Aprenda a fazer slides com a plataforma Canva" },
+    { index: 2, title: "Quiz dinâmico", description: "Aprenda a fazer quiz do tipo Kahoot para a sala de aula" },
+    { index: 3, title: "Boas anotações", description: "Aprenda a tomar notas de maneira eficiente" }
+  ];
 }
